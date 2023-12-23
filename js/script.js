a429f5c2b437d8c0637e319bd77f70f7146539ce
AnaliApps <ronoanali5@gmail.com>
2023-12-23 12:36:11 +0300

    Add refresh page function
    I have added a refresh page so that the hover color can change to a different color.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
-let btn = document.querySelector(".btn");
+let btn = document.createElement("button");
+btn.setAttribute("class","btn")
+btn.textContent = "SetGrid"
+let btnrefreshPage = document.createElement("button");
+btnrefreshPage.setAttribute("class","btn");
+btnrefreshPage.textContent = "RefreshPage";
+let btnDv = document.createElement("div")
+btnDv.setAttribute("id","btn")
 let body = document.querySelector("body");
+let container = document.createElement("div");
+container.setAttribute("class","container")
 let red = Math.floor(Math.random()* 255);
 let green = Math.floor(Math.random()* 255);
 let blue = Math.floor(Math.random()* 255);
-console.log(red);
 function createDivs(n){
+    let h = 500/n;
+    let w = 500/n;
     console.log(n)
     for(let i=0;i<n;i++){
         let row = document.createElement("div");
@@ -12,8 +22,8 @@ function createDivs(n){
         for(let j=0;j<n;j++){
             let cell = document.createElement("div");
             cell.className = "colDivs";
-            cell.style.height = "16px";
-            cell.style.width = "16px";
+            cell.style.height = `${h}px`;
+            cell.style.width = `${w}px`;
             cell.style.backgroundColor = "blue";
             cell.addEventListener("mouseover",()=>{
                 cell.style.backgroundColor = `rgb(${red},${green},${blue})`;
@@ -25,18 +35,28 @@ function createDivs(n){
             console.log(`${j}`);
             row.appendChild(cell);
         }
-        body.appendChild(row)
+        container.appendChild(row)
     }
+    
+    body.appendChild(container);
 }
 
 function run(){
-    body.innerHTML = "";
+    container.innerHTML = "";
     let num = parseInt(prompt("Enter grid number you want: "));
     if(num > 100){
         num = parseInt(prompt("Enter grid number you want: "));
         return
     }
     createDivs(num);
+    
 }
 
+function refreshPage(){
+    window.location.reload();
+}
+btnDv.appendChild(btn);
+btnDv.appendChild(btnrefreshPage)
+body.appendChild(btnDv);
 btn.addEventListener("click",run);
+btnrefreshPage.addEventListener("click",refreshPage);

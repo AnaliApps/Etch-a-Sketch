8b2651643a3311351f2f9bf3b89cb9a650719925
AnaliApps <ronoanali5@gmail.com>
2023-12-23 11:02:04 +0300

    Random background color
    I have added random color for each interactions of the grid squares. Whenever a new grid is created the background color on mouse hover is different each time.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 let btn = document.querySelector(".btn");
 let body = document.querySelector("body");
-
+let red = Math.floor(Math.random()* 255);
+let green = Math.floor(Math.random()* 255);
+let blue = Math.floor(Math.random()* 255);
+console.log(red);
 function createDivs(n){
     console.log(n)
     for(let i=0;i<n;i++){
@@ -13,7 +16,7 @@ function createDivs(n){
             cell.style.width = "16px";
             cell.style.backgroundColor = "blue";
             cell.addEventListener("mouseover",()=>{
-                cell.style.backgroundColor = "purple";
+                cell.style.backgroundColor = `rgb(${red},${green},${blue})`;
             });
             cell.addEventListener("mouseout",()=>{
                 cell.style.backgroundColor = "blue";
